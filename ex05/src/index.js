d057d3642a3eaf21cc3b75d2e36e498e400615f1
hokula <adelhokula18@gmail.com>
2021-03-17 00:11:38 +0100

    Inequality and Strict Inequality Operator

---
+++ ex05/src/index.js
@@ -0,0 +1,15 @@
+function compareStrictValues(m) {
+  if (m != 55) {
+    return "Not equal";
+  } else {
+    return "Equal";
+  }
+}
+compareStrictValues();
+console.log(compareStrictValues(55));
+console.log(compareStrictValues("55"));
+console.log(compareStrictValues("21"));
+console.log(compareStrictValues(12));
+console.log(compareStrictValues("Doe"));
+
+module.exports = compareStrictValues;
