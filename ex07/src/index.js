f42825331d88d20ebbb44940effba5f634da549b
hokula <adelhokula18@gmail.com>
2021-03-17 00:14:47 +0100

    Comparison with the Logical And Operator

---
+++ ex07/src/index.js
@@ -0,0 +1,14 @@
+function logicalAndOperator(num) {
+  if (num >= 40 && num <= 80) {
+    return "Yes";
+  }
+  return "No";
+}
+logicalAndOperator();
+console.log(logicalAndOperator(0));
+console.log(logicalAndOperator(24));
+console.log(logicalAndOperator(45));
+console.log(logicalAndOperator(57));
+console.log(logicalAndOperator(100));
+
+module.exports = logicalAndOperator;
